1e900e5fdd0208b03703e142b6fabbe20e49a3f8
Gabriel Negrão Silva <itsg_negrao@hotmail.com>
2017-09-22 19:34:57 +0000

    Inicio Parte 4

---
+++ src/Parte4/NewClass.java
@@ -0,0 +1,27 @@
+/*
+ Faça um servidor para processar as seguintes mensagens dos clientes. O servidor deve suportar mensagens de múltiplos clientes. Use o TCP.
+As mensagens estão no formato String UTF:
+TIME
+* Retorna a hora do sistema como uma String UTF no formato HH:MM:SS
+DATE
+* Retorna a data do sistema como uma String UTF no formato DD/MM/AAAA
+FILES
+* Retorna os arquivos da pasta definida por padrao (p. ex. /home/user/shared)
+* retorna um inteiro indicando o número de arquivos
+* envia o nome de um arquivo por vez como uma String UTF
+DOWN nome-arquivo
+* Faz o download do arquivo nome-arquivo
+* retorna 0 se nome não existe ou retorna o tamanho do arquivo
+* lê o número de bytes indicado por tamanho do arquivo e grava em um diretório padrão
+EXIT
+* Finaliza a conexão
+ */
+package Parte4;
+
+/**
+ *
+ * @author itsgnegrao
+ */
+public class NewClass {
+    
+}
---
+++ src/Parte5/NewClass.java
@@ -0,0 +1,14 @@
+/*
+Fazer uma aplicação com um servidor que gerencia a comunicação entre dois clientes usando TCP. 
+Para cada cliente é criada uma thread no servidor. 
+A comunicação entre as threads deve usar recursos como pipe ou memória compartilhada.
+ */
+package Parte5;
+
+/**
+ *
+ * @author itsgnegrao
+ */
+public class NewClass {
+    
+}
